99e5cf63e529d627e6712c9e04eef97b5a73dd11
David Prior Hope <david.prior-hope17@imperial.ac.uk>
2021-12-27 23:17:07 +0000

    comment from python script

--- super.py
+++ super.py
@@ -10,9 +10,19 @@ import os
 import time
 
 
+#sense
 exec(open("./sense.py").read())
+
+#upload data
 exec(open("./upload.py").read())
-exec(open("./clean_assets.py").read())
+#exec(open("./clean_assets.py").read())
+
+#Create website assets:
+    #Read data 
+    #Do processing (multi-dimensional linear regression)
+        #this includes calculating the number of layers to cycle right now
+    #build chart
+
 exec(open("./chart_build.py").read())
 exec(open("./daily_chart_build.py").read())
 print('Successfully generated charts')
@@ -20,45 +30,9 @@ print('Successfully generated charts')
 directory = 'docs/assets/'
 files = os.listdir(directory)
 
-
-while len(files)<3:                 #this while loop is to stall out until all 3 graphs have been uploaded to the folder
+#this while loop is to stall out until all 3 graphs have been uploaded to the folder
+while len(files)<3:
     files =os.listdir(directory)
-    time.sleep(1)
-    print('1 loop')
 
+#pushing updated files to github
 exec(open("./push_to_github.py").read())
-
-
-#Collect data
-#sense
-
-#Upload data
-#upload
-
-#Create website assets:
-    #Read data 
-    #Do processing (multi-dimensional linear regression)
-        #this includes calculating the number of layers to cycle right now
-    #build chart
-#clean_assets
-'''chart_build
-daily_chart_build
-print('Successfully generated charts')
-
-
-#push updates to github
-
-directory = 'docs/assets/'
-
-'''
-
-#files = os.listdir(directory)
-
-#print(len(files))
-
-'''while len(files)<3:                 #this while loop is to stall out until all 3 graphs have been uploaded to the folder
-    files =os.listdir(directory)
-    time.sleep(1)
-    print('1 loop')
-
-push_to_github'''
