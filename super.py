b2a979c921ca18fa1e4f4af409abf7fdcdffae2a
David Prior Hope <david.prior-hope17@imperial.ac.uk>
2021-12-27 23:26:22 +0000

    comment from python script

--- super.py
+++ super.py
@@ -1,10 +1,14 @@
 #Hello this is the main file which should be run for each cycle
 
+import os
+
+
 #sense
 exec(open("./sense.py").read())
 
 #upload data
 exec(open("./upload.py").read())
+
 
 #Create website assets:
     #Read data 
@@ -12,10 +16,22 @@ exec(open("./upload.py").read())
         #this includes calculating the number of layers to cycle right now
     #build chart
 
+#exec(open("./clean_assets.py").read())
+
 exec(open("./chart_build.py").read())
 exec(open("./daily_chart_build.py").read())
 print('Generated charts')
 
 
+
+directory = 'docs/assets/'
+files = os.listdir(directory)
+
+#this while loop is to stall out until all 3 graphs have been uploaded to the folder
+'''while len(files)<3:
+    files =os.listdir(directory)'''
+
 #pushing updated files to github
 exec(open("./push_to_github.py").read())
+
+print('The website changes are being deployed and the website should update in the next few minutes.')
