0369e6bf08ffeb01a154e0f3268fd36f21e87926
David Prior Hope <david.prior-hope17@imperial.ac.uk>
2021-12-27 23:04:41 +0000

    comment from python script

--- super.py
+++ super.py
@@ -1,15 +1,33 @@
 #Hello this is the main file which should be run for each cycle
 
-import push_to_github
+'''import push_to_github
 import sense
 import upload
 import chart_build
 import daily_chart_build
-import clean_assets
+import clean_assets'''
 import os
 import time
 
-print('test')
+
+exec(open("./sense.py").read())
+exec(open("./upload.py").read())
+exec(open("./clean_assets.py").read())
+exec(open("./chart_build.py").read())
+exec(open("./daily_chart_build.py").read())
+print('Successfully generated charts')
+
+directory = 'docs/assets/'
+files = os.listdir(directory)
+
+
+while len(files)<3:                 #this while loop is to stall out until all 3 graphs have been uploaded to the folder
+    files =os.listdir(directory)
+    time.sleep(1)
+    print('1 loop')
+
+exec(open("./push_to_github.py").read())
+
 
 #Collect data
 #sense
