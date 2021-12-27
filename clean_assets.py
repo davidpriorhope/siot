4cd10259d717b7a5c2c1094c3d34fa67f06c666e
David Prior Hope <david.prior-hope17@imperial.ac.uk>
2021-12-27 23:21:03 +0000

    comment from python script

--- clean_assets.py
+++
@@ -1,8 +0,0 @@
-import os
-
-directory = 'docs/assets/'
-
-files = os.listdir(directory)
-
-for i in files:
-    os.remove(directory +i)
--- super.py
+++ super.py
@@ -1,21 +1,10 @@
 #Hello this is the main file which should be run for each cycle
 
-'''import push_to_github
-import sense
-import upload
-import chart_build
-import daily_chart_build
-import clean_assets'''
-import os
-import time
-
-
 #sense
 exec(open("./sense.py").read())
 
 #upload data
 exec(open("./upload.py").read())
-#exec(open("./clean_assets.py").read())
 
 #Create website assets:
     #Read data 
@@ -25,14 +14,8 @@ exec(open("./upload.py").read())
 
 exec(open("./chart_build.py").read())
 exec(open("./daily_chart_build.py").read())
-print('Successfully generated charts')
+print('Generated charts')
 
-directory = 'docs/assets/'
-files = os.listdir(directory)
-
-#this while loop is to stall out until all 3 graphs have been uploaded to the folder
-while len(files)<3:
-    files =os.listdir(directory)
 
 #pushing updated files to github
 exec(open("./push_to_github.py").read())
